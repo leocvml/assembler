5f9b1ba73c1322688bdbfb0003377221d6990f63
Leo Cheng <leo0988365660@gmail.com>
2017-04-26 23:18:49 +0800

    Update asmnof.py

--- asmnof.py
+++ asmnof.py
@@ -6,12 +6,24 @@ def hexi(List): #int
     for i in List:
         Delx.append(i[2:])
     return Delx
-    '''
-    for i in  Listlen:
-        Delx.append(str[i[2:]])
+def fit6(opcode):
+    new_opcode =[]
+    for i in opcode:
+        if len(i) < 6 and i !=' ':
+            fit0 = "000000"
+            fit0 = fit0[0:6-len(i)]+i
+            new_opcode.append(fit0)
+        else:
+            new_opcode.append(i)      
+    return new_opcode
+def Out_txt(LocCounter,INSFromL,Op):
+    file = open('asm_out.txt','w')
+    txt_Line = range(len(INSFromL))
+    for i in txt_Line:
+        file.write(LocCounter[i]+"\t"+INSFromL[i][0] +"\t" + INSFromL[i][1]+"\t" +INSFromL[i][2]+"\t" + INSFromL[i][3]+"\t"+Op[i]+"\n")
+      
+    file.close()
     
-    return (Delx)
-'''
 def run(filename):
     LabelsP3 = ['LDA','LDB','LDX','ADD','JSUB','JEQ','STA','TIX','RSUB','J','JLT','WORD','END']
     P3_Opcode = ['00','68','04','18','48','30','0C','2C','4C','3C','38']
@@ -47,7 +59,7 @@ def run(filename):
                 if (instr[3][0]) == 'C':
                     LocCounter += len(instr[3]) - 3
                     LocCList.append(hex(LocCounter))
-                elif (instr[3][0]) == 'B':
+                elif (instr[3][0]) == 'X':
                     LocCounter += int((len(instr[3]) - 3) / 2)
                     LocCList.append(hex(LocCounter))
                 else:
@@ -69,24 +81,31 @@ def run(filename):
   #  print(Label[:numberOfLines])
 
     OpCode = []
+    OpCode.append(" ")
     for Stament in INSFromL:
         if Stament[2] in LabelsP3 and Stament[2] != 'END':
             if Stament[2] == 'WORD':
                 WORDop =[]
                 WORDop.append(hex(int(Stament[3])))
                 WORDop = hexi(WORDop)
-                print(''.join(c for c in WORDop))
+                #print(''.join(c for c in WORDop))
+                OpCode.append(''.join(c for c in WORDop))
             elif Stament[3] in Label:                
                 if Stament[3] == ' ':
-                    print ("BUG1")
+                    #print ("BUG1")
+                    OpCode.append("BUG1")
                 else:
-                    print (P3_Opcode[LabelsP3.index(Stament[2])] + LocCList[Label.index(Stament[3])])
+                    #print (P3_Opcode[LabelsP3.index(Stament[2])] + LocCList[Label.index(Stament[3])])
+                    OpCode.append(P3_Opcode[LabelsP3.index(Stament[2])] + LocCList[Label.index(Stament[3])])
             elif Stament[3].split(',')[1] == 'X':
                 temp = LocCList[Label.index(Stament[3].split(',')[0])]
-                op_string = str(hex(int(temp[0])+8)) +temp[1:len(temp)]
-                print((P3_Opcode[LabelsP3.index(Stament[2])]) + op_string) 
+                temp_hex = str(hex(int(temp[0])+8))
+                op_string = temp_hex[2:len(temp_hex)] +temp[1:len(temp)]
+                #print((P3_Opcode[LabelsP3.index(Stament[2])]) + op_string)
+                OpCode.append((P3_Opcode[LabelsP3.index(Stament[2])]) + op_string)
             else:
-                print("BUG2")
+                #print("BUG2")
+                OpCode.append("BUG2")
                 
                  
         elif Stament[2] in LabelsOther:
@@ -99,8 +118,10 @@ def run(filename):
                     for i in range(toasciiLen):
                         BYTEop.append(hex(int(''.join(str(ord(toascii[i]))))))
                     BYTEop = hexi(BYTEop)
-                    print (''.join(c for c in BYTEop ))
+                    #print (''.join(c for c in BYTEop ))
+                    OpCode.append(''.join(c for c in BYTEop ))
                 elif (Stament[3][0]) == 'X':
+                    '''
                     end = len(Stament[3])
                     toascii = Stament[3][2:end-1]       #del'B'
                     toasciiLen = len(toascii)
@@ -108,25 +129,17 @@ def run(filename):
                     for i in range(toasciiLen):
                         BYTEop.append(hex(int(''.join(str(ord(toascii[i]))))))
                     BYTEop = hexi(BYTEop)
-                    print (''.join(c for c in BYTEop ))
-
-
-
-                    
+                    #print (''.join(c for c in BYTEop ))
                     '''
-            elif Stament[2] == 'WORD':
-                WORDop = []
-                WORDop.append(hex(int(Stament[3])))
-                print(WORDop)
-                               #Stament[3][2:end-1]     #print (''.join(str(ord()) for c in toascii)) 
+                    OpCode.append(Stament[3][2:len(Stament[3])-1])
+            else:
+                OpCode.append(" ")
+    OpCode.append(" ")
+    OpCode = fit6(OpCode)
+    print(OpCode)
+    Out_txt(LocCList,INSFromL,OpCode)
+                    
 
-                    #print (str(ord(c)) for c in instr[3])
-               
-               # elif (instr[3][0]) == 'B':
-               #     LocCounter += int((len(instr[3]) - 3) / 2)
-                #    LocCList.append(hex(LocCounter))            
-        '''
-  
   
 
 
